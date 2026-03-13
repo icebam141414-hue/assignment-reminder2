9eac773fd8c9709bf563b199ad529640ac0d4d1f
Thunchanok <icebam141414@gmail.com>
2026-03-13 09:45:01 +0700

    fix error

--- line-bot/index.js
+++ line-bot/index.js
@@ -353,5 +353,5 @@ if (diff > 0 && diff <= hours24) {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log("Server running on port " + PORT); 
 });
